ec40fcf5eec8b1106dd69ed7a329e66dc583ff1e
30perk <perikingayuk7@gmail.com>
2024-07-12 20:54:05 -0500

    poll added

--- src/Heapin.java
+++ src/Heapin.java
@@ -7,6 +7,7 @@ public class Heapin {
 
     public Heapin() {
         minheap = new ArrayList<>();
+        size = 0;
     }
 
     public int getSize() {
@@ -25,9 +26,9 @@ public class Heapin {
 
     public void add(int value) {
         minheap.add(value);
-        size += 1;
+        size ++;
 
-        int currIndex = minheap.size() -1;
+        int currIndex = size -1;
         int parentIndex = (currIndex - 1) /2;
 
         while (currIndex > 0 && minheap.get(currIndex) < minheap.get(parentIndex)) {
@@ -42,12 +43,46 @@ public class Heapin {
 
     }
 
-    public void poll() {
-        if (size == 0) {
+    public int poll() {
+        if (isEmpty()) {
             throw new IllegalArgumentException("Heap is Empty");
+
         }
 
-        //
+
+        // hold onto root
+        int tempRoot = minheap.get(0);
+        // get last element
+        int lastAdded = minheap.remove(size-1);
+        size --;
+
+        if (!(isEmpty()) ) {
+            minheap.set(0, lastAdded);
+            // bring down
+            int currIndex = 0;
+            int leftIndex = (2* currIndex +1);
+            int rightIndex = (2 * currIndex +2);
+
+            while ( leftIndex < minheap.size() ) {
+                int smallestChild = leftIndex;
+
+                if ( rightIndex < minheap.size() && minheap.get(rightIndex) < minheap.get(leftIndex) ) {
+                    smallestChild = rightIndex; }
+
+                if ( minheap.get(currIndex) <= minheap.get(smallestChild) ) {
+                    break; }
+
+                int temp = minheap.get(currIndex);
+                minheap.set(currIndex, minheap.get(smallestChild));
+                minheap.set(smallestChild, temp);
+
+                currIndex = smallestChild;
+                leftIndex = (2 * currIndex + 1);
+                rightIndex = (2 * currIndex +2);
+            }
+
+        }
+        return tempRoot;
     }
 
 
@@ -55,11 +90,18 @@ public class Heapin {
     public static void main(String[] args) {
         Heapin hi = new Heapin();
         hi.add(1);
-        hi.add(1);
-        hi.add(1);
-        hi.add(1);
-
+        hi.add(2);
+        hi.add(3);
+        hi.add(4 );
+        hi.poll();
+        hi.poll();
+        hi.poll();
+        hi.poll();
         System.out.println(hi.getSize());
+
+        hi.poll();
+
+
         System.out.println(hi.isEmpty());
 
     }
--- src/HeapinTest.java
+++ src/HeapinTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HeapinTest {
 
@@ -42,5 +43,64 @@ public class HeapinTest {
         assertEquals(6, getSize);
         assertEquals(134, peeking);
     }
+
+    @Test
+    public void addWithPoll() {
+        Heapin hi = new Heapin();
+        hi.add(4);
+        hi.add(3423);
+        hi.poll();
+        hi.add(3);
+
+        int peeking = hi.peek();
+        int getSize = hi.getSize();
+        assertEquals(2, getSize);
+        assertEquals(3, peeking);
+        assertEquals(2, hi.getSize());
+    }
+
+    @Test
+    public void addWithPollMoreNums() {
+        Heapin hi = new Heapin();
+        hi.add(4);
+        hi.poll();
+        hi.add(3423);
+        hi.add(133);
+        hi.poll();
+        hi.add(3);
+        hi.add(1);
+        hi.poll();
+        hi.add(6535);
+
+        int peeking = hi.peek();
+        int getSize = hi.getSize();
+        assertEquals(3, getSize);
+        assertEquals(3, peeking);
+        assertEquals(3, hi.getSize());
+    }
+
+    @Test
+    public void addWithPollMultipleTimes() {
+        Heapin hi = new Heapin();
+        hi.add(4);
+        hi.add(3423);
+        hi.poll();
+        hi.add(3);
+        hi.poll();
+        hi.poll();
+        hi.poll();
+        hi.poll();
+        hi.poll();
+        hi.poll();
+
+        int peeking = hi.peek();
+        int getSize = hi.getSize();
+        thrown.expect(IllegalArgumentException.class);
+        thrown.expectMessage("Heap is Empty"); // Optionally check the exception message
+        hi.peek();
+        assertEquals(0, getSize);
+        assertEquals(0, hi.getSize());
+    }
+
 
 }
